6ffcf209145bc061b6d840c8de10fa3bc0566e6e
Eddie <dwa79@sfu.ca>
2016-06-06 03:54:58 +0000

    asd

--- app/assets/javascripts/students.js
+++
@@ -1,9 +0,0 @@
-function about(){
-	var pn=window.alert("Developer Info: \nName: Di Wang(Eddie)\nCurrently studying at: Simon Fraser University\nEmail: dwa79@sfu.ca\nStudent Number: 301257741\nTel: 778-892-3858(Canada)\n\nFeel free to contact me if you have further questions!");
-
-	}
-}
-
-function col(){
-	window.open("http://www.sfu.ca");
-}
